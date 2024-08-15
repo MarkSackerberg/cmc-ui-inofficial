006faa22d62716e80e2f5c0e58899d2dfef2a4de
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2024-08-15 11:28:51 +0200

    fix blockhash

--- components/mintButton.tsx
+++ components/mintButton.tsx
@@ -151,7 +151,7 @@ const mintClick = async (
         isClosable: true,
       });
       const latestBlockhash = (await umi.rpc.getLatestBlockhash({commitment: "finalized"}));
-      routeBuild.setBlockhash(latestBlockhash)
+      routeBuild = routeBuild.setBlockhash(latestBlockhash)
       await umi.rpc
       .sendTransaction(routeBuild.build(umi), { skipPreflight:true, maxRetries: 1, preflightCommitment: "finalized", commitment: "finalized" })
     }
